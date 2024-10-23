447502b2f8874c013ee8fd01e1851d87988669ed
Juan Marcos Lorenzo <jmlorenzo1993u@gmail.com>
2024-10-23 18:07:38 -0300

    cambio de directorio

--- api-router.php
+++ api-router.php
@@ -14,6 +14,7 @@ $router->addRoute('pedidos', 'POST', 'pedidosController', 'addPedido');
 $router->addRoute('pedidos/:ID', 'PUT', 'pedidosController', 'updatePedido'); 
 $router->addRoute('pedidos/token', 'GET', 'UserApiController', 'getToken');
 
+echo $_GET["resource"];
 
 // ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
